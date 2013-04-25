5ba6aa15f34bbb6f607a760fd10d91e95ba1d0a3
DAM <DAM@aula1_05>
2013-04-25 12:10:10 +0200

    oscar

---
+++ src/xikle/MacOs.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xikle;
+
+/**
+ *
+ * @author DAM
+ */
+public class MacOs extends ClasePadre {
+    //Hugo es gay
+    
+}
