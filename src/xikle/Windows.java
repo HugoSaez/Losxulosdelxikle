6ac1b8f33c8dc072d3529df21db8c0e089a19e0e
DAM <DAM@AULA1_01>
2013-04-25 12:10:41 +0200

    Clase windows pablo

---
+++ src/xikle/Windows.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xikle;
+
+/**
+ *
+ * @author DAM
+ */
+public class Windows extends ClasePadre  {
+
+    public Windows() {
+    }
+    
+}
