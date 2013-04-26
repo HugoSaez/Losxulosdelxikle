284976ad593a5e10513ad897cabb612826ac4497
DAM <DAM@aula1_05>
2013-04-26 12:32:44 +0200

    MacOs terminado --- oscar

--- src/xikle/MacOs.java
+++ src/xikle/MacOs.java
@@ -4,12 +4,37 @@
  */
 package xikle;
 
+import java.util.Random;
+
 /**
  *
  * @author DAM
  */
 public abstract class MacOs extends ClasePadre {
-         public MacOs(String version, int ramMaxima, int bits, String copiar, String pegar) {
-         super(version, ramMaxima, bits, copiar, pegar);
+    
+         public MacOs(String version, int ramMaxima, int bits) {
+         super(version, ramMaxima, bits);
          }
+
+    @Override
+    public String copiar(String copiado) {
+        return super.copiar(copiado); 
+    }
+
+    @Override
+    public String pegar(String pegado) {
+        return super.pegar(pegado); 
+    }
+    
+    public String pantallazoAzul (String texto){
+        Random r = new Random(5);
+        if (r.nextInt()==0) return "Hay pantallazo";
+        else return "No hay pantallazo";        
+    }
+    public String buscarVirus (String texto){
+        Random r = new Random(10);
+        if (r.nextInt()==0) return "Hay virus";
+        else return "No hay virus";        
+    }
+    
 }
