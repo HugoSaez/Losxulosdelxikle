59c93f979e0b8595269cffccb25cf583fdde475c
OFI <OFI@AULA1_6>
2013-04-26 12:26:25 +0200

    clase linux terminada

--- src/xikle/Linux.java
+++ src/xikle/Linux.java
@@ -4,23 +4,35 @@
  */
 package xikle;
 
+import java.util.Random;
+
 /**
  *
  * @author OFI
  */
-public class Linux extends ClasePadre{
-    public Linux (String version, int ramMaxima, int bits){
-        super (version, ramMaxima, bits);
-    }
-
-    @Override
-    public String Copiar(String copiado) {
-        return super.Copiar(copiado);
-    }
-
-    @Override
-    public String Pegar(String pegado) {
-        return super.Pegar(pegado);
-    }
+public abstract class Linux extends ClasePadre{
     
+    public Linux (String version,int ramMaxima, int bits,String copiar,String pegar){
+        super(version, ramMaxima, bits, copiar, pegar);
+    }
+
+    @Override
+    public String copiar(String copiado) {
+        return super.copiar(copiado);
+    }
+
+    @Override
+    public String pegar(String pegado) {
+        return super.pegar(pegado);
+    }
+    public String buscarVirus(String texto){
+        Random r = new Random(4);
+        if (r.nextInt()==0) return "Virus encontrado";
+        else return "Virus no encontrado";
+    }
+    public String pantallazoAzul (String texto){
+        Random r = new Random(2);
+        if (r.nextInt()==0) return "Ha habido un pantallazo azul";
+        else return "No ha habido ningun pantallazo azul";
+    }
 }
